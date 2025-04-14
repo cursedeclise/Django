675c14c339d93a34c70c153d64e7f0887211120f
cursedeclise <pl110709@wgcloud.org>
2025-04-14 12:15:14 -0500

    changed some things

--- djangotutorial/ctf/models.py
+++ djangotutorial/ctf/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+class challenge(models.Model):
+    name=models.CharField(max_length=255)
+    summary=models.TextField()
+    flag_value=models.CharField(max_length=255, unique=True)
+    points=models.IntegerField(default=10)
+
+    def __str__(self):
+        return self.name
+
+class Participant(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    flags_solved = models.ManyToManyField(challenge, blank=True, related_name='completed_by')
+    total_points = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.user.username
+
+    def update_points(self):
+        self.total_points = sum(challenge.points for challenge in self.flags_solved.all())
+        self.save()
--- djangotutorial/ctf/views.py
+++ djangotutorial/ctf/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
 def home(request):
     return render(request, 'index.html')
-    
+
 def challenges(request):
-    return render(request, 'challenges.html')
+    #get data
+    challenges=challenge.objects.all()
+    #send data as context var
+
+
+    return render(request, 'challenges.html',{
+        'challenges': challenges,
+    })
