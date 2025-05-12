6bb2c94d46bf02213316449a537c4a463209e3a4
cursedeclise <pl110709@wgcloud.org>
2025-05-12 12:16:06 -0500

    h

--- djangotutorial/ctf/forms.py
+++ djangotutorial/ctf/forms.py
@@ -1,7 +1,7 @@
 from django import forms
-
 from .models import *
-
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.models import User
 
 class ChallengeForm(forms.ModelForm):
     class Meta:
@@ -10,3 +10,12 @@ class ChallengeForm(forms.ModelForm):
 
 class FlagSubmissionForm(forms.Form):
     flag_value=forms.CharField(max_length=100, label="Enter Flag")
+
+class CustomUserCreationForm(UserCreationForm):
+    email=forms.EmailField(required=True)
+    first_name=forms.CharField(max_length=30, required=True)
+    last_name=forms.CharField(max_length=30, required=True)
+
+    class Meta:
+        model=User
+        fields-("username", "email", "first_name", "last_name", "password1", "password2")
--- djangotutorial/ctf/models.py
+++ djangotutorial/ctf/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils import timezone
+from django.core.exceptions import ValidationError
 
 # Create your models here.
 class Challenge(models.Model):
@@ -31,3 +33,28 @@ class ChallengeCompletion(models.Model):
 
     def __str__(self):
         return f"{self.participant.user.username} completed {self.challenge.name}"
+
+
+
+class Operation(models.Model):
+    running=models.BooleanField(default=False)
+    timer=models.CharField(max_length=40, default='', verbose_name="Timer (March 18, 2025 15:37:25)")
+    registration_window_open=models.BooleanField(default=False)
+
+    def clean(self):
+        try:
+            timezone.datetime.striptime(self.timer, "%B %d, %Y %H:%:M:%S")
+        except ValueError:
+            raise ValidationError({
+                'timer': 'Timer must be in the format "March 18, 2025 15:37:25".'
+            })
+
+class Notification(models.Model):
+    title=models.CharField(max_length=255)
+    message=models.TextField()
+    created_at=models.DateTimeField(auto_now_add=True)
+    running=models.BooleanField(default=False)
+    color=models.CharField(max_length=7, default='#007bff')
+
+    def __str__(self):
+        return self.title
